b7a451abf0bab109db732f673e390c1fa52738b3
Pedro Muniz <joao.munnyz@gmail.com>
2024-08-30 13:28:58 -0300

    Repetições

---
+++ Javascript/aula 13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+
+do  {
+    console.log(`passo ${c}`)
+    c++ //c = c + 1
+} while (6 >= c )
+
+/*
+var c = 1
+while (6 >= c ) {
+    console.log(`passo ${c}`)
+    c++ //c = c + 1
+} */
+
+/*
+console.log('teste')
+console.log('teste')
+console.log('teste')
+console.log('teste')
+console.log('teste')
+console.log('teste')
+*/
