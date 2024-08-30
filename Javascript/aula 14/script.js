02ac9845352dcfba2fab004fa897aaf3d1d0edad
Pedro Muniz <joao.munnyz@gmail.com>
2024-08-30 13:54:48 -0300

    for

---
+++ Javascript/aula 14/script.js
@@ -0,0 +1,15 @@
+ /*
+ var c =1 
+ while ( 5 >= c) { 
+    console.log('ola')
+    c = c+1
+ }
+
+ */
+
+ //---------
+console.log('Vai começar ...')
+ for (var c =1 ; 5 >= c ; c = c+1 ) {
+    console.log(c)
+ }
+ console.log(`Terminou`)
