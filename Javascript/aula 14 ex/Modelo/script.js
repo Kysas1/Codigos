050f1d6fd3955db9bce030b922dba0708d635147
Pedro Muniz <joao.munnyz@gmail.com>
2024-09-10 16:40:02 -0300

    exercicio tabuada

--- Javascript/aula 14 ex/Modelo/script.js
+++ Javascript/aula 14 ex/Modelo/script.js
@@ -1,1 +1,26 @@
-console.log('Olá')
+function tabuada() {
+    let num = document.getElementById('txtn1')
+    let tab = document.getElementById('seltab')
+    if (num.value.length == 0 ){
+        window.alert('Por Favor, Digite um numero')
+    } else {
+        let n = Number(txtn1.value)
+        let c = 1
+         tab.innerHTML = ''
+        while (c <=10 ){
+            let item = document.createElement('option')
+            item.text = ` ${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            c++
+            tab.appendChild(item)
+        }
+    }
+}
+
+function limpe() {
+    let tab = document.getElementById('seltab')
+    let item = document.createElement('option')
+    tab.innerHTML = ''
+    item.text = 'Digite um Valor'
+    tab.appendChild(item)
+}
